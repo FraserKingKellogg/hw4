eab735bdc52045b1caf6d5b8a8a35c6f315cdc2c
FraserKingKellogg <fraser.king@kellogg.northwestern.edu>
2022-05-28 15:11:25 +0000

    Update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,21 +1,23 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+      @places = Place.where({"user_id" => @current_user["id"]})
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+      @place = Place.find_by({ "id" => params["id"] })
+      @posts = Post.where({ "place_id" => @place["id"] })
   end
 
   def new
     @place = Place.new
+
   end
 
   def create
     if @current_user 
     @place = Place.new
+    @place["user_id"] = @current_user["id"] 
     @place["name"] = params["place"]["name"]
     @place.save
   else
--- db/migrate/20220528144157_create_places.rb
+++ db/migrate/20220528144157_create_places.rb
@@ -2,8 +2,9 @@ class CreatePlaces < ActiveRecord::Migration[7.0]
   def change
     create_table :places do |t|
       t.string "name"
-
+      t.integer "user_id"
       t.timestamps
     end
   end
 end
+
