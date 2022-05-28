507e81403abb02895bd028d11f4d6bdd8851b824
FraserKingKellogg <fraser.king@kellogg.northwestern.edu>
2022-05-28 03:52:50 +0000

    Keep_Going

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_09_020701) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.string "image"
     t.date "posted_on"
     t.integer "place_id"
     t.integer "user_id"
