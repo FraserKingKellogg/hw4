e134c39eda7dacb8e9a4b972b7c8301b8293dbfe
FraserKingKellogg <fraser.king@kellogg.northwestern.edu>
2022-05-28 02:13:33 +0000

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
 
   def new
     @post = Post.new
@@ -6,12 +9,16 @@ class PostsController < ApplicationController
   end
 
   def create
+    if @current_user
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post.save
+    else
+      flash["notice"] = "Login First."
+    end
     redirect_to "/places/#{@post["place_id"]}"
 end
 end
